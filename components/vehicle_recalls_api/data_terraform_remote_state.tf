b964e71d37688f26532264515a4dca356778d7e1
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2017-11-08 16:09:39 +0100

    Making build_and_deploy_lambda universal

--- components/vehicle_recalls_api/data_terraform_remote_state.tf
+++ components/vehicle_recalls_api/data_terraform_remote_state.tf
@@ -1,24 +1,24 @@
 data "terraform_remote_state" "mgmt" {
   backend = "s3"
 
-# vehicle-recalls-terraformscaffold-054631451206-eu-west-1
   config {
-    bucket = "${format(
-      "%s-terraformscaffold-%s-%s",
-      'vehicle-recalls',
-      '054631451206',
-      'eu-west-1'
-    )}"
+    bucket = "vehicle-recalls-terraformscaffold-054631451206-eu-west-1"
+        # "${format(
+        #   ,
+        #   "vehicle-recalls",
+        #   "054631451206",
+        #   "eu-west-1"
+        # )}"
+    key = "vehicle-recalls/054631451206/eu-west-1/dawidm/fake_smmt.tfstate"
+    # key = "${format(
+    #   "%s/%s/%s/%s/%s.tfstate",
+    #   "vehicle-recalls",
+    #   "054631451206",
+    #   "eu-west-1",
+    #   "dawidm",
+    #   "fake_smmt"
+    # )}"
 
-    key = "${format(
-      "%s/%s/%s/%s/%s.tfstate",
-      'vehicle-recalls',
-      '054631451206',
-      'eu-west-1',
-      'dawidm',
-      'fake_smmt'
-    )}"
-
-    region = "${lookup(var.mgmt, "region")}"
+    region = "eu-west-1"
   }
 }
