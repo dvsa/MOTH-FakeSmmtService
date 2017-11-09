b340aa5ea1cf29b8ee189f8797942400d12bbaac
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2017-11-09 18:45:31 +0100

    Introducing group variables

--- jenkinsfiles/app_build.groovy
+++ jenkinsfiles/app_build.groovy
@@ -55,31 +55,8 @@ for (repo in github.keySet()) {
   }
 }
 
-// TODO Remove this after rework.
-TF_LOG_LEVEL = 'ERROR'
-TF_PROJECT = 'vehicle-recalls'
-BUCKET_PREFIX = 'uk.gov.dvsa.vehicle-recalls.'
-
-def sh_output(String script) {
-  return sh(
-    script: script,
-    returnStdout: true
-  ).trim()
-}
-
-def sh_status(String script) {
-  return sh(
-    script: script,
-    returnStatus: true
-  )
-}
-
 def log_info(String info) {
   echo "[INFO] ${info}"
-}
-
-def bucket_exists(String bucket) {
-  return sh_status("aws s3 ls s3://${bucket} --region ${AWS_REGION} 2>&1 | grep -q -e \'NoSuchBucket\' -e \'AccessDenied\'")
 }
 
 def verify_or_create_bucket(String bucket_prefix, String tf_component) {
@@ -119,102 +96,6 @@ Boolean buildNPM(
   }
 }
 
-def build_and_upload_js(bucket,build_id) {
-
-
-    return
-    return dist_file
-  }
-
-
-def abort_build(String message) {
-  currentBuild.result = 'ABORTED'
-  error(message)
-}
-
-def copy_file_to_s3(file, bucket) {
-  sh("aws s3 cp $file s3://$bucket")
-}
-
-def checkout_github_repo(String group, String repo, String branch) {
-  checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', depth: 0, noTags: true, reference: '', shallow: true], [$class: 'RelativeTargetDirectory', relativeTargetDir: repo]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/' + group + '/' + repo + '.git']]]
-}
-
-def checkout_github_repo_branch_or_master(group, repo, branch) {
-  try {
-    checkout_github_repo(group, repo, branch)
-  } catch(error) {
-    log_info("${error}")
-    checkout_github_repo(group, repo, "master")
-  }
-}
-
-def checkout_gitlab_repo(String group, String repo, String branch) {
-  log_info("Checkout GITLAB repo \"${group}/${repo}\" branch \"${branch}")
-
-  creds = '313a82d3-f2e7-4787-837e-7517f3ce84eb'
-  checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', depth: 0, noTags: true, reference: '', shallow: true], [$class: 'RelativeTargetDirectory', relativeTargetDir: repo]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: creds, url: 'git@gitlab.motdev.org.uk:' + group + '/' + repo + '.git']]]
-}
-
-def checkout_gitlab_repo_branch_or_master(group, repo, branch) {
-  try {
-    checkout_gitlab_repo(group, repo, branch)
-  } catch(error) {
-    log_info("${error}")
-    checkout_gitlab_repo(group, repo, "master")
-  }
-}
-
-def tf_scaffold(action, component, extra_args, returnStdout=false) {
-  log_info("Terraform ${action} on \"${component}\" with \"${extra_args}\"")
-
-  env_type = 'FB'
-
-  withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: env_type + '_AWS_CREDENTIALS', usernameVariable: 'aws_key_id', passwordVariable: 'aws_secret_key']]) {
-    withEnv([
-      "AWS_DEFAULT_REGION=${AWS_REGION}",
-      "AWS_ACCESS_KEY_ID=${env.aws_key_id}",
-      "AWS_SECRET_ACCESS_KEY=${env.aws_secret_key}",
-      'PATH+=/opt/tfenv/bin',
-      "TF_LOG=${TF_LOG_LEVEL}"
-    ]) {
-      return sh(
-        script: """
-        export TFENV_DEBUG=0
-        bash -x recalls-infrastructure/bin/terraform.sh \
-          --action ${action} \
-          --project ${TF_PROJECT} \
-          --environment ${ENV} \
-          --component ${component} \
-          --region ${AWS_REGION} \
-          -- ${extra_args}
-        """,
-        returnStdout: returnStdout
-      )
-    }
-  }
-}
-
-def tf_output(variable, tf_component) {
-  //returning last line of terraform scaffold output
-  return tf_scaffold("output ${variable}", tf_component, "", true).split("\n")[-1]
-}
-
-def populate_tfvars(key, value) {
-  log_info("Populating tfvars \"${key}\" with \"${value}\"")
-
-  tfvars_path = "env_${AWS_REGION}_${ENV}.tfvars"
-
-  sh("sed -i 's|%${key}%|${value}|g' recalls-infrastructure/etc/${tfvars_path}")
-}
-
-def get_tfenv() {
-  if (!fileExists('/opt/tfenv/bin/tfenv')) {
-    dir('/opt') { check_out_github_repo('cartest', 'tfenv', 'master') }
-  } else {
-    log_info('TFENV already installed. Skipping...')
-  }
-}
 
 def build_and_deploy_lambda(params) {
   String name                   = params.name
@@ -301,20 +182,6 @@ def build_and_deploy_lambda(params) {
               'apply'
             )
           }
-          // return
-          // sh('ls -la')
-          //
-          // if(tfvars) {
-          //   tfvars.each { entry ->
-          //     populate_tfvars(entry.key, entry.value)
-          //   }
-          // }
-          // populate_tfvars("lambda_s3_key", dist)
-          //
-          // tf_scaffold('plan', tf_component, "")
-          // tf_scaffold('apply', tf_component, "")
-          //
-          // return tf_output('api_gateway_url', tf_component)
         }
       }
 
@@ -348,7 +215,7 @@ node(jenkinsctrl_node_label&&account) {
               build_number,
               's3',
               bucket_prefix,
-              'plan' // When devs agree on this change we will change plan to apply.
+              'apply' // When devs agree on this change we will change plan to apply.
             )
           }
         }
@@ -357,24 +224,23 @@ node(jenkinsctrl_node_label&&account) {
   }
 
 node('builder') {
-    // fake_smmt_url = build_and_deploy_lambda(
-    //   name: 'Fake SMMT',
-    //   bucket_prefix: bucket_prefix,
-    //   repo: github.fake_smmt.url,
-    //   tf_component: 'fake_smmt',
-    //   code_branch: brach,
-    //   environment: environment,
-    //   awsFunctionsFactory: awsFunctionsFactory,
-    //   repoFunctionsFactory: repoFunctionsFactory,
-    //   globalValuesFactory: globalValuesFactory,
-    //   github: github,
-    //   gitlab: gitlab,
-    //   build_id: build_id,
-    //   jenkinsctrl_node_label: jenkinsctrl_node_label,
-    //   account: account,
-    //   project: project
-    // )
-    // return
+    fake_smmt_url = build_and_deploy_lambda(
+      name: 'Fake SMMT',
+      bucket_prefix: bucket_prefix,
+      repo: github.fake_smmt.url,
+      tf_component: 'fake_smmt',
+      code_branch: brach,
+      environment: environment,
+      awsFunctionsFactory: awsFunctionsFactory,
+      repoFunctionsFactory: repoFunctionsFactory,
+      globalValuesFactory: globalValuesFactory,
+      github: github,
+      gitlab: gitlab,
+      build_id: build_id,
+      jenkinsctrl_node_label: jenkinsctrl_node_label,
+      account: account,
+      project: project
+    )
     build_and_deploy_lambda(
       name: 'Vehicle Recalls',
       bucket_prefix: bucket_prefix,
