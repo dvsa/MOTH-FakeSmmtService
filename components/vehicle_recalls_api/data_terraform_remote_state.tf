dd472627eb921e9bf800127881928dc15f221720
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2017-11-09 12:45:05 +0100

    Introducing group variables

--- components/vehicle_recalls_api/data_terraform_remote_state.tf
+++ components/vehicle_recalls_api/data_terraform_remote_state.tf
@@ -2,22 +2,22 @@ data "terraform_remote_state" "fake_smmt" {
   backend = "s3"
 
   config {
-    bucket = "vehicle-recalls-terraformscaffold-054631451206-eu-west-1"
-        # "${format(
-        #   ,
-        #   "vehicle-recalls",
-        #   "054631451206",
-        #   "eu-west-1"
-        # )}"
-    key = "vehicle-recalls/054631451206/eu-west-1/dawidm/fake_smmt.tfstate"
-    # key = "${format(
-    #   "%s/%s/%s/%s/%s.tfstate",
-    #   "vehicle-recalls",
-    #   "054631451206",
-    #   "eu-west-1",
-    #   "dawidm",
-    #   "fake_smmt"
-    # )}"
+    # bucket = "vehicle-recalls-terraformscaffold-054631451206-eu-west-1"
+    bucket = "${format(
+          %s-terraformscaffold-%s-%s,
+          var.project,
+          var.account,
+          data.aws_region.current.name
+        )}"
+    # key = "vehicle-recalls/054631451206/eu-west-1/dawidm/fake_smmt.tfstate"
+    key = "${format(
+      "%s/%s/%s/%s/%s.tfstate",
+      var.project,
+      var.account,
+      data.aws_region.current.name,
+      var.environment,
+      var.fake_smmt_component_name
+    )}"
 
     region = "eu-west-1"
   }
---
+++ etc/group_dev.tfvars
@@ -0,0 +1,4 @@
+bucket_prefix = "uk.gov.dvsa.vehicle-recalls."
+account = "054631451206"
+project = "vehicle-recalls"
+fake_smmt_component_name = "fake_smmt"
