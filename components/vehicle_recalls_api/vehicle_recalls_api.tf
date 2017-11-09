248c7cff20bf88d655af0ca9260a2c69b558ad5c
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2017-11-09 11:48:37 +0100

    lambda_env_vars

--- components/vehicle_recalls_api/vehicle_recalls_api.tf
+++ components/vehicle_recalls_api/vehicle_recalls_api.tf
@@ -12,7 +12,7 @@ module "vehicle_recalls_api" {
   lambda_timeout            = "15"
   lambda_ver                = "$LATEST"
   lambda_env_vars           = {
-    SMMT_API_URI = "${data.terraform_remote_state.fake_smmt.api_gateway_url}/vincheck"
+    SMMT_API_URI = "/vincheck"
     SMMT_API_KEY = "localApiKey"
   }
 }
