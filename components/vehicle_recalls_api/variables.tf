3c32ba338defe66779013c35545a984c4fe59c0c
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2017-11-10 11:10:53 +0100

    Execute permission

--- components/vehicle_recalls_api/variables.tf
+++ components/vehicle_recalls_api/variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
   default = "eu-west-1"
 }
 
-variable "project" {
-  type    = "string"
-  default = "vehicle_recalls"
-}
-
 variable "bucket_prefix" {
   type  = "string"
 }
